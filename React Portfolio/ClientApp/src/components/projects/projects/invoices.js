33f30b323f696f3644a5c9ac28b4f84759e209f2
antoinehr <antoine.hlgr@gmail.com>
2023-12-29 21:18:26 +0100

    add project page

--- React Portfolio/ClientApp/src/components/projects/projects/invoices.js
+++ React Portfolio/ClientApp/src/components/projects/projects/invoices.js
@@ -1,28 +1,89 @@
-﻿import React from "react"
+﻿import React, { useEffect, useState } from "react"
 import Salesforce from '../../../assets/salesforce.png';
-import Excel from '../../../assets/excel.png';
-import Jira from '../../../assets/jira.png';
-import PowerBi from '../../../assets/powerbi.png';
-import Tableau from '../../../assets/tableau.png';
-import Web from '../../../assets/web.png';
-import Zendesk from '../../../assets/zendesk.png';
 import { useTranslation } from "react-i18next";
-
+import moment from "moment";
 
 
 function Invoices(props) {
 
     const { t, i18n } = useTranslation();
+    const [invoices, setInvoices] = useState({})
+
+    useEffect(() => {
+        const fetchReleaseData = async () => {
+            try {
+                const response = await fetch('https://api.github.com/repos/savemyfiles/antoine-hoellinger.cloud', {
+                    headers: {
+                        Authorization: 'token  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 ',
+                    },
+                });
+                const data = await response.json();
+                setInvoices(data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données de release :', error);
+            }
+        };
+
+        fetchReleaseData();
+    }, []);
 
     return (
-        <div>
+        <section>
             <div className="title">
                 <h1>{t("Mes projets personnels")}</h1>
                 <div className="subline"></div>
             </div>
+            <h2>EasyInvoice</h2>
+
+            <div className='project-container'>
+
+                <div className='project-section-container'>
+
+                    <div>
+                        <img style={{maxWidth: '500px'}}  src={Salesforce} />
+                    </div>
+
+                <div style={{ textAlign: 'left' }}>
+
+                    <div className='project-side-description'>
+                        <h3>{t("Information sur le projet")}</h3>
+
+                         <p>{t("Ma passion pour l'entrepreneuriat m'a conduit à créer une solution innovante pour répondre à un besoin concret : la gestion efficace des factures pour les auto-entrepreneurs en France. En tant qu'indépendant, j'ai rencontré des difficultés à trouver une plateforme répondant pleinement à mes besoins, alors j'ai décidé de concevoir ma propre application.")} </p>
+
+                        <div>
+                                <h3>{t("Origine et Decription du Projet")} </h3>
+                                <p>{t("Partant de mon propre besoin de créer, gérer et stocker des factures, j'ai développé une application qui simplifie cette tâche cruciale pour tout entrepreneur. Cette initiative découle d'une lacune sur le marché : l'absence d'une solution intuitive et complète pour les auto-entrepreneurs français.")} </p>
+                        </div>
+
+                        <div>
+                            <p>{t("Mon application offre une interface intuitive pour créer des factures professionnelles en quelques étapes simples. Elle permet également de gérer plusieurs entreprises depuis une même plateforme, garantissant un suivi précis des paiements et une organisation optimale. Le stockage sécurisé des factures offre un accès immédiat à toute donnée importante, à tout moment.Connectivité ClientUn aspect essentiel de cette application est sa capacité à établir des liens directs avec les clients. Elle facilite un suivi transparent des factures, favorisant ainsi des échanges fluides et une meilleure communication pour un service client de qualité.L'Engagement envers la SimplicitéMon objectif principal était de créer une solution facile à utiliser et personnalisable selon les besoins spécifiques de chaque utilisateur. L'application s'adapte à l'identité de l'entreprise, offrant ainsi une expérience fluide et intuitive.")} </p>
+                            </div>
+
+                            <div>
+                                <h3>{t("Information Github")}</h3>
+                                <ul style={{ color: 'white' }}>
+                                    <li>{t("Nom du projet")} : {invoices.name}</li>
+                                    <li>{t("Language utilisé")} : {invoices.language}</li>
+                                    <li>{t("Date de création")} : {moment(invoices.pushed_at).format('DD MMMM YYYY')}</li>
+                                    <li>{t("Dernière mise à jour")} : {moment(invoices.updated_at).format('DD MMMM YYYY')}</li>
+                                </ul>
+
+                                {!invoices.private && <a className="button" href={invoices.html_url}>{t("Voir sur")} GitHub</a>}
+
+                            </div>
+
+                    </div>
+
+                
+
+               </div>
+               </div>
 
 
-        </div>
+            </div>
+
+
+        </section>
      
         )
     }
